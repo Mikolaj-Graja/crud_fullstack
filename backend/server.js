c18e1ec9dc2bf3ccf2e8e5c8ff8c9fba5b4740ab
Mikołaj Graja <mikolajgraja@gmail.com>
2020-07-10 21:08:48 +0200

    port from secret.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
-const {connection} = require('./secret');
+const {connection, port} = require('./secret');
 const bodyParser = require('body-parser');
-const port = 3000;
 
 app.use(bodyParser.json());
 connection.connect();
