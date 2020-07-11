af78dfaae62f673a87ba9d1178ea3d8943edf0ed
Mikołaj Graja <mikolajgraja@gmail.com>
2020-07-11 16:36:32 +0200

    delete and put users working on server

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan("dev"));
 
@@ -24,11 +25,31 @@ app.get(`/`, (req, res) => {
 
   app.post(`/registerUser`, (req, res) => {
     const { userName } = req.body;
-    if (!userName) return;
+    if (!userName) return res.send("No userName");
     connection.query(`INSERT INTO users (userName) VALUES (?);`, userName, err => {
         if (err) throw err;
-        console.log(`${userName} INSERTED`);  
+        console.log(`${userName} INSERTED`);
+        res.send(`${userName} INSERTED`)  
     });
+});
+
+app.put('/modifyUser/:id', (req, res) => {
+  const { id } = req.params;
+  const newUserName = req.body.userName;
+  if (!newUserName) return res.send("No new userName");
+  connection.query('UPDATE users SET userName = ? WHERE id = ?', [newUserName, id], err => {
+    if (err) throw err;
+    console.log(`you changed userName to ${newUserName} in row number ${id}`);
+  });
+});
+
+app.delete('/deleteUser/:id', (req, res) => {
+  const { id } = req.params;
+  if (!id) return res.send("No id");
+  connection.query('DELETE FROM users WHERE id = ?', id, (err, rows, fields) => {
+    if (err) throw err;
+    console.log(`you delete row number: ${id}`);
+  });
 });
 
 app.listen(port, err => {
